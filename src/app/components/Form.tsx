04aca299ffef5162861321b5c58fdd1c3af20f7a
mahmoudafattah98 <128329870+mahmoudafattah98@users.noreply.github.com>
2024-08-15 12:23:11 +0300

    comitting after separation of concerns & useState hook.

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,43 +1,52 @@
 'use client';
-import Image from "next/image";
-import styles from "./page.module.css";
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Checkbox, Form as AntForm, Input } from 'antd';
 
-
-    interface FormProps{ 
-    onSubmit : () => void;
-
+interface FormProps {
+  onSubmit: (values: FieldType) => void;
 }
 
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: boolean;
+};
 
-    
-export default function Form({ onSubmit}: FormProps) {
-  type FieldType = {
-    username?: string;
-    password?: string;
-    remember?: boolean;
-    };
+export default function Form({ onSubmit }: FormProps) {
+const[username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+    console.log(e.target.value)
+  };
 
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+    console.log(e.target.value)
+  };
 
-    
-    return (
-        <AntForm<FieldType>
-            name="basic"
-        onFinish=(onSubmit)
+  const handleFinish = () => {
+    onSubmit({ username, password, remember: true });
+  };
+
+  return (
+    <>
+      <AntForm<FieldType>
+        name="basic"
+        onFinish={handleFinish}
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
         style={{ maxWidth: 600 }}
         initialValues={{ remember: true }}
         autoComplete="off"
-        
       >
         <AntForm.Item
           label="Username"
           name="username"
           rules={[{ required: true, message: 'Please input your username!' }]}
         >
-          <Input />
+          <Input onChange={handleUsernameChange}/>
         </AntForm.Item>
 
         <AntForm.Item
@@ -45,7 +54,7 @@ export default function Form({ onSubmit}: FormProps) {
           name="password"
           rules={[{ required: true, message: 'Please input your password!' }]}
         >
-          <Input.Password />
+          <Input.Password onChange={handlePasswordChange} />
         </AntForm.Item>
 
         <AntForm.Item
@@ -57,11 +66,11 @@ export default function Form({ onSubmit}: FormProps) {
         </AntForm.Item>
 
         <AntForm.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit" onClick={}>
+          <Button type="primary" htmlType="submit">
             Submit
           </Button>
         </AntForm.Item>
       </AntForm>
-    )
-
- }
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import React from 'react';
-import Form from "./components/Form.tsx"
+import Form from "./components/Form";
 
 export default function Home() {
 
+  const handleSubmit = () => {
+    console.log("Hi");
+  };
+
   return (
     <main>
-     <Form/>
+      <Form onSubmit={handleSubmit} />
     </main>
   );
 }
