251c52c43b91969a71aad65cb2630f0ac9b7867e
mahmoudafattah98 <128329870+mahmoudafattah98@users.noreply.github.com>
2024-08-18 16:35:42 +0300

    entered the no data prompt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,6 @@ import Form from "./components/Form";
 import Movies from "./components/Movies";
 import styles from "./page.module.css";
 export default function Home() {
-  const handleSubmit = () => {
-    console.log("Hi");
-  };
-
   return (
     <main className={styles.main}>
       {/* <Form onSubmit={handleSubmit} /> */}
